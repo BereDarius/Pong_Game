47c2eda800168b5bcb4e8fe081941cb7cfc9504c
Bere Darius <tuborg.bere3080@gmail.com>
2019-11-12 20:11:38 +0200

    Changes done to resolution and added visible border

--- pong.py
+++ pong.py
@@ -1,10 +1,11 @@
 import turtle
 import winsound
 
+# Screen setup
 wn = turtle.Screen()
 wn.title("Pong")
 wn.bgcolor("black")
-wn.setup(width=800, height=600)
+wn.setup(width=1.0, height=1.0)
 wn.tracer(0)
 
 # Score
@@ -18,7 +19,7 @@ paddle_a.shape("square")
 paddle_a.color("white")
 paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
-paddle_a.goto(-350, 0)
+paddle_a.goto(-wn.window_width()//2.5, 0)
 
 # Paddle B
 paddle_b = turtle.Turtle()
@@ -27,7 +28,7 @@ paddle_b.shape("square")
 paddle_b.color("white")
 paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
-paddle_b.goto(350, 0)
+paddle_b.goto(wn.window_width()//2.5, 0)
 
 # Ball
 ball = turtle.Turtle()
@@ -39,13 +40,28 @@ ball.goto(0, 0)
 ball.dx = 1
 ball.dy = 1
 
+# Border drawing
+border = turtle.Turtle()
+border.speed(0)
+border.color("white")
+border.pensize(5)
+border.penup()
+border.goto(-wn.window_width()//2.3-9, wn.window_height()//2.5-2)
+border.pendown()
+border.goto(wn.window_width()//2.3+9, wn.window_height()//2.5-2)
+border.goto(wn.window_width()//2.3+9, -wn.window_height()//2.5+2)
+border.goto(-wn.window_width()//2.3-9, -wn.window_height()//2.5+2)
+border.goto(-wn.window_width()//2.3-9, wn.window_height()//2.5-2)
+border.penup()
+border.hideturtle()
+
 # Pen
 pen = turtle.Turtle()
 pen.speed(0)
 pen.color("white")
 pen.penup()
 pen.hideturtle()
-pen.goto(0, 260)
+pen.goto(0, wn.window_height()//2.4)
 pen.write("Player A: 0  Player B: 0",
           align="center",
           font=("Courier", 24, "normal"))
@@ -56,25 +72,49 @@ pen.write("Player A: 0  Player B: 0",
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
-    paddle_a.sety(y)
+    if not (paddle_a.ycor()+60 > wn.window_height()//2.5-2):
+        paddle_a.sety(y)
 
 
 def paddle_a_down():
     y = paddle_a.ycor()
     y -= 20
-    paddle_a.sety(y)
+    if not (paddle_a.ycor()-60 < -wn.window_height()//2.5+2):
+        paddle_a.sety(y)
 
 
 def paddle_b_up():
     y = paddle_b.ycor()
     y += 20
-    paddle_b.sety(y)
+    if not (paddle_b.ycor()+60 > wn.window_height()//2.5-2):
+        paddle_b.sety(y)
 
 
 def paddle_b_down():
     y = paddle_b.ycor()
     y -= 20
-    paddle_b.sety(y)
+    if not (paddle_b.ycor()-60 < -wn.window_height()//2.5+2):
+        paddle_b.sety(y)
+
+
+def goodbye():
+    wn.bye()
+
+
+def ball_speed_reset():
+    ball.dx = 1
+    ball.dy = 1
+
+
+def ball_speed_up():
+    if (ball.dx < 0):
+        ball.dx -= 0.3
+    if (ball.dx > 0):
+        ball.dx += 0.3
+    if (ball.dy < 0):
+        ball.dy -= 0.3
+    if (ball.dy > 0):
+        ball.dy += 0.3
 
 
 # Keyboard binding
@@ -83,6 +123,7 @@ wn.onkeypress(paddle_a_up, 'w')
 wn.onkeypress(paddle_a_down, 's')
 wn.onkeypress(paddle_b_up, 'Up')
 wn.onkeypress(paddle_b_down, 'Down')
+wn.onkeypress(goodbye, 'Escape')
 
 # Main game loop
 while True:
@@ -93,20 +134,21 @@ while True:
     ball.sety(ball.ycor() + ball.dy)
 
     # Border checking
-    if ball.ycor() > 290:
-        ball.sety(290)
+    if ball.ycor() > wn.window_height()//2.5:
+        ball.sety(wn.window_height()//2.5)
         ball.dy *= -1
         winsound.PlaySound(
             "pong_blip", winsound.SND_ASYNC | winsound.SND_FILENAME)
 
-    if ball.ycor() < -290:
-        ball.sety(-290)
+    if ball.ycor() < -wn.window_height()//2.5:
+        ball.sety(-wn.window_height()//2.5)
         ball.dy *= -1
         winsound.PlaySound(
             "pong_blip", winsound.SND_ASYNC | winsound.SND_FILENAME)
 
-    if ball.xcor() > 390:
+    if ball.xcor() > wn.window_width()//2.3:
         ball.goto(0, 0)
+        ball_speed_reset()
         ball.dx *= -1
         score_a += 1
         pen.clear()
@@ -114,8 +156,9 @@ while True:
                   align="center",
                   font=("Courier", 24, "normal"))
 
-    if ball.xcor() < -390:
+    if ball.xcor() < -wn.window_width()//2.3:
         ball.goto(0, 0)
+        ball_speed_reset()
         ball.dx *= -1
         score_b += 1
         pen.clear()
@@ -125,23 +168,25 @@ while True:
 
     # Paddle and ball collisions
     if (
-        ball.xcor() > 340 and
-        ball.xcor() < 350 and
+        ball.xcor() > paddle_b.xcor()-20 and
+        ball.xcor() < paddle_b.xcor()+2 and
         ball.ycor() < paddle_b.ycor() + 40 and
         ball.ycor() > paddle_b.ycor() - 40
     ):
-        ball.setx(340)
+        ball.setx(paddle_b.xcor()-20)
         ball.dx *= -1
         winsound.PlaySound(
             "pong_blip", winsound.SND_ASYNC | winsound.SND_FILENAME)
+        ball_speed_up()
 
     if (
-        ball.xcor() < -340 and
-        ball.xcor() > -350 and
+        ball.xcor() < paddle_a.xcor()+20 and
+        ball.xcor() > paddle_a.xcor()-2 and
         ball.ycor() < paddle_a.ycor() + 40 and
         ball.ycor() > paddle_a.ycor() - 40
     ):
-        ball.setx(-340)
+        ball.setx(paddle_a.xcor()+20)
         ball.dx *= -1
         winsound.PlaySound(
             "pong_blip", winsound.SND_ASYNC | winsound.SND_FILENAME)
+        ball_speed_up()
